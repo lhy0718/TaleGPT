d0440867b2bd86119226be6c75445c8ca7b40134
Hanyong Lee <glhy0718@gmail.com>
2024-06-10 20:36:52 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from threading import Thread
 
@@ -21,21 +21,21 @@ if torch.cuda.is_available():
         device_map="auto",
         load_in_8bit=True,
         revision="8bit",
-        # torch_dtype=torch.float16,
     )
+else:
+    model = AutoModelForCausalLM.from_pretrained(MODEL)
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+with open("prompt.txt") as f:
+    prompt = f.read()
 
 
 class StopOnTokens(StoppingCriteria):
     def __call__(
         self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
     ) -> bool:
-        stop_ids = [
-            tokenizer.convert_tokens_to_ids("<|endoftext|>"),
-            tokenizer.convert_tokens_to_ids("<"),
-            tokenizer.convert_tokens_to_ids("#"),
-        ]  # indices of the tokens that we want to stop on
+        stop_ids = [2, 6, 31]  # "<|endoftext|>": 2, "#": 6, "<": 31
         for stop_id in stop_ids:
             if input_ids[0][-1] == stop_id:
                 return True
@@ -43,26 +43,24 @@ class StopOnTokens(StoppingCriteria):
 
 
 def convert_history_item_to_message(history_item: list) -> str:
-    # return f"### 명령어:\n{history_item[0].strip()}\n### 출력:\n{history_item[1].split('#')[0].strip()}"
-    return f"### 제목: {history_item[0].strip()}\n### 출력: {history_item[1].split('#')[0].strip()}"
+    return f"### 제목:\n{history_item[0].strip()}\n### 판타지 동화 출력:\n옛날 옛적에{history_item[1].split('#')[0].strip()}"
 
 
-def answer(message, history, top_p, top_k, temperature):
-    with open("prompt.txt") as f:
-        prompt = f.read()
-    pre_system_message = prompt
+def answer(user_input, history, top_p, top_k, temperature):
     curr_system_message = """
 ### 명령어:
-다음 제목의 동요를 생성하세요.
+다음 명령에 대한 아동을 타겟으로 하는 동화 또는 검/마법/기사/요정 등이 난무하는 판타지 소설을 출력합니다.
 """
 
     stop = StopOnTokens()
 
-    message = message or ""
-    history_transformer_format = history + [[message, ""]]
+    user_input = user_input or ""
+    history_transformer_format = history + [
+        [user_input, ""]
+    ]  # history: [[title, story], ...]
 
     messages = (
-        pre_system_message
+        prompt
         + "\n".join(
             map(convert_history_item_to_message, history_transformer_format[:-1])
         )
@@ -73,19 +71,12 @@ def answer(message, history, top_p, top_k, temperature):
     print(messages)
 
     model_inputs = tokenizer(
-        [messages],
-        # return_token_type_ids=False,
-        return_tensors="pt",
+        [messages], return_token_type_ids=False, return_tensors="pt"
     )
-    print(model_inputs["input_ids"].shape, model.config)
     if torch.cuda.is_available():
         model_inputs = model_inputs.to("cuda")
     streamer = TextIteratorStreamer(
-        tokenizer,
-        timeout=10.0,
-        skip_prompt=True,
-        skip_special_tokens=True,
-        return_token_type_ids=False,
+        tokenizer, timeout=10.0, skip_prompt=True, skip_special_tokens=True
     )
     generate_kwargs = dict(
         model_inputs,
@@ -131,11 +122,11 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
         chatbot=chatbot,
         textbox=textbox,
         examples=[
-            ["달님"],
-            ["용이 나타났다!"],
-            ["가족의 사랑"],
+            ["금화를 지키는 용"],
+            ["마법 동물원의 비밀"],
+            ["전설의 검"],
         ],
-        title="LyricGPT",
+        title="TaleGPT (동화 / 판타지 소설 생성 인공지능)",
         description="© 중앙대학교 기계학습자동화연구실 - CAU AutoML Lab",
         submit_btn="제출",
         stop_btn="멈춤",
