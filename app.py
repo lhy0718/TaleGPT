51ba46c8c849b61f640cd2120b73ae03dab383e3
Hanyong Lee <glhy0718@gmail.com>
2024-08-22 15:24:25 +0900

    app update

--- app.py
+++ app.py
@@ -1,70 +1,30 @@
-import os
-import random
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-
 import gc
-import json
 from threading import Thread
 
 import gradio as gr
 import torch
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    BitsAndBytesConfig,
-    StoppingCriteria,
-    StoppingCriteriaList,
-    TextIteratorStreamer,
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+
+MODEL = "LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
 )
 
-INITIAL_PROMPT = "### 명령어:\n다음 명령에 대한 아동을 타겟으로 하는 동화 또는 각종 판타지 요소가 난무하는 판타지 소설을 출력합니다.\n"
-GEN_PROMPT_FORMAT = "### 제목:\n{title}\n### 판타지 동화 출력:\n{story}"
-CONTINUEW_GEN_PROMPT_FORMAT = "### 이전과 이어서 동화 계속 출력:\n{story}"
 NO_INPUT_WARNING = "아무것도 입력하지 않았습니다. 동화 제목을 입력해주세요."
-
-MODEL = "beomi/KoAlpaca-Polyglot-12.8B"  # 제일 좋음
-# MODEL = "beomi/Solar-Ko-Recovery-11B"
-# MODEL = "beomi/OPEN-SOLAR-KO-10.7B"
-# MODEL = "beomi/Llama-3-Open-Ko-8B"
-# MODEL = "MLP-KTLim/llama-3-Korean-Bllossom-8B"
-
-if torch.cuda.is_available():
-    quantization_config = BitsAndBytesConfig(load_in_8bit=True)
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL,
-        device_map="auto",
-        quantization_config=quantization_config,
-    )
-else:
-    model = AutoModelForCausalLM.from_pretrained(MODEL)
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
+INITIAL_PROMPT = "입력되는 제목을 바탕으로 대한 아동을 타겟으로 하는 동화 또는 각종 판타지 요소가 난무하는 판타지 소설을 출력합니다."
+CONTINUE_PROMPT = "출력이 끝난 부분부터 동화를 이어서 출력합니다."
 
 
-class StopOnTokens(StoppingCriteria):
-    def __call__(
-        self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
-    ) -> bool:
-        stop_ids = [tokenizer.eos_token_id] + tokenizer.convert_tokens_to_ids(
-            ["#", "<"]
-        )
-        for stop_id in stop_ids:
-            if input_ids[0][-1] == stop_id:
-                return True
-        return False
-
-
-def convert_history_item_to_message(history_item: list) -> str:
-    title, story = history_item
-    if story == NO_INPUT_WARNING:
-        return ""
-    title = title or ""
-    story = story or ""
-    if title == "":
-        return CONTINUEW_GEN_PROMPT_FORMAT.format(story=story)
-    return GEN_PROMPT_FORMAT.format(title=title, story=story)
+def convert_history_to_messages(history: list = []) -> list:
+    messages = [{"role": "system", "content": INITIAL_PROMPT}]
+    for user, assistant in history:
+        messages.append({"role": "user", "content": user})
+        messages.append({"role": "assistant", "content": assistant})
+    return messages
 
 
 def answer(user_input, history, top_p, top_k, temperature):
@@ -74,35 +34,24 @@ def answer(user_input, history, top_p, top_k, temperature):
         gc.collect()
         torch.cuda.empty_cache()
 
-        with open("dataset.json") as f:
-            fewshot = random.choices(json.load(f), k=5)
-            history = fewshot + history
+        if user_input.strip() == "":  # 이전 동화를 이어서 출력
+            history = convert_history_to_messages(history[-4:])
+            history.append({"role": "system", "content": CONTINUE_PROMPT})
+        else:  # 새로운 동화 생성
+            history = convert_history_to_messages()
+            history.append({"role": "user", "content": user_input})
 
-        stop = StopOnTokens()
-
-        history += [[user_input, ""]]  # history: [[title, story], ...]
-
-        input_text = INITIAL_PROMPT + "\n".join(
-            [
-                convert_history_item_to_message(item) for item in history[-6:]
-            ]  # 최근 5개의 fewshot + user_input
-        )
-
-        print("\n========== Input Messages")
-        print(input_text)
-
-        model_inputs = tokenizer(
-            [input_text], return_token_type_ids=False, return_tensors="pt"
-        )
-        if torch.cuda.is_available():
-            model_inputs = model_inputs.to("cuda")
+        input_ids = tokenizer.apply_chat_template(
+            history, tokenize=True, add_generation_prompt=True, return_tensors="pt"
+        ).to("cuda")
 
         streamer = TextIteratorStreamer(
             tokenizer, timeout=10.0, skip_prompt=True, skip_special_tokens=True
         )
 
         generate_kwargs = dict(
-            model_inputs,
+            inputs=input_ids,
+            mask_token_id=tokenizer.mask_token_id,
             pad_token_id=tokenizer.eos_token_id,
             streamer=streamer,
             max_new_tokens=512,
@@ -111,17 +60,15 @@ def answer(user_input, history, top_p, top_k, temperature):
             top_k=top_k,
             temperature=temperature,
             num_beams=1,
-            stopping_criteria=StoppingCriteriaList([stop]),
         )
 
         t = Thread(target=model.generate, kwargs=generate_kwargs)
         t.start()
 
         partial_message = ""
-        for new_token in streamer:
-            if new_token not in ["<", "#"]:
-                partial_message += new_token
-                yield partial_message
+        for new_text in streamer:
+            partial_message += new_text
+            yield partial_message
 
 
 # Interface
@@ -141,6 +88,7 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
         0, 5000, value=2000, label="단어 선택의 가짓수", render=False
     )
     temperature_slider = gr.Slider(0, 1, value=1, label="창의성", render=False)
+    stop_btn = gr.Button("멈춤", render=False, variant="stop")
 
     gr.ChatInterface(
         answer,
@@ -161,4 +109,7 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
         additional_inputs=[top_p_slider, top_k_slider, temperature_slider],
     )
 
-demo.queue().launch(share=True, auth=("automl", "208217"))
+demo.queue(default_concurrency_limit=2).launch(
+    # share=True,
+    # auth=("automl", "208217"),
+)
